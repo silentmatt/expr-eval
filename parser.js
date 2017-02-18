181f83dc43622c749bec983a8d8d1da5da57ef22
Matthew Crumley <email@matthewcrumley.com>
2017-02-18 16:24:29 -0500

    Move Expression class into a module

--- parser.js
+++ parser.js
@@ -12,65 +12,7 @@
 import { TEOF } from './src/token';
 import { TokenStream } from './src/token-stream';
 import { ParserState } from './src/parser-state';
-import simplify from './src/simplify';
-import substitute from './src/substitute';
-import expressionToString from './src/expression-to-string';
-import getSymbols from './src/get-symbols';
-import evaluate from './src/evaluate';
-
-function Expression(tokens, parser) {
-  this.tokens = tokens;
-  this.parser = parser;
-  this.unaryOps = parser.unaryOps;
-  this.binaryOps = parser.binaryOps;
-  this.ternaryOps = parser.ternaryOps;
-  this.functions = parser.functions;
-}
-
-Expression.prototype.simplify = function (values) {
-  values = values || {};
-  return new Expression(simplify(this.tokens, this.unaryOps, this.binaryOps, this.ternaryOps, values), this.parser);
-};
-
-Expression.prototype.substitute = function (variable, expr) {
-  if (!(expr instanceof Expression)) {
-    expr = this.parser.parse(String(expr));
-  }
-
-  return new Expression(substitute(this.tokens, variable, expr), this.parser);
-};
-
-Expression.prototype.evaluate = function (values) {
-  values = values || {};
-  return evaluate(this.tokens, this, values);
-};
-
-Expression.prototype.toString = function () {
-  return expressionToString(this.tokens, false);
-};
-
-Expression.prototype.symbols = function () {
-  var vars = [];
-  getSymbols(this.tokens, vars);
-  return vars;
-};
-
-Expression.prototype.variables = function () {
-  var vars = [];
-  getSymbols(this.tokens, vars);
-  var functions = this.functions;
-  return vars.filter(function (name) {
-    return !(name in functions);
-  });
-};
-
-Expression.prototype.toJSFunction = function (param, variables) {
-  var expr = this;
-  var f = new Function(param, 'with(this.functions) with (this.ternaryOps) with (this.binaryOps) with (this.unaryOps) { return ' + expressionToString(this.simplify(variables).tokens, true) + '; }'); // eslint-disable-line no-new-func
-  return function () {
-    return f.apply(expr, arguments);
-  };
-};
+import { Expression } from './src/expression';
 
 function add(a, b) {
   return Number(a) + Number(b);

11461cd293c20ac13f26d1996aafaa646387de01
Matthew Crumley <email@matthewcrumley.com>
2017-02-18 16:24:22 -0500

    Move expressionToString function into a module

--- parser.js
+++ parser.js
@@ -16,6 +16,7 @@ import { ParserState } from './src/parser-state';
 import contains from './src/contains';
 import simplify from './src/simplify';
 import substitute from './src/substitute';
+import expressionToString from './src/expression-to-string';
 
 function Expression(tokens, parser) {
   this.tokens = tokens;
@@ -24,13 +25,6 @@ function Expression(tokens, parser) {
   this.binaryOps = parser.binaryOps;
   this.ternaryOps = parser.ternaryOps;
   this.functions = parser.functions;
-}
-
-function escapeValue(v) {
-  if (typeof v === 'string') {
-    return JSON.stringify(v).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
-  }
-  return v;
 }
 
 Expression.prototype.simplify = function (values) {
@@ -116,95 +110,6 @@ Expression.prototype.evaluate = function (values) {
   values = values || {};
   return evaluate(this.tokens, this, values);
 };
-
-function expressionToString(tokens, toJS) {
-  var nstack = [];
-  var n1, n2, n3;
-  var f;
-  for (var i = 0, L = tokens.length; i < L; i++) {
-    var item = tokens[i];
-    var type = item.type;
-    if (type === INUMBER) {
-      if (typeof item.value === 'number' && item.value < 0) {
-        nstack.push('(' + item.value + ')');
-      } else {
-        nstack.push(escapeValue(item.value));
-      }
-    } else if (type === IOP2) {
-      n2 = nstack.pop();
-      n1 = nstack.pop();
-      f = item.value;
-      if (toJS) {
-        if (f === '^') {
-          nstack.push('Math.pow(' + n1 + ', ' + n2 + ')');
-        } else if (f === 'and') {
-          nstack.push('(!!' + n1 + ' && !!' + n2 + ')');
-        } else if (f === 'or') {
-          nstack.push('(!!' + n1 + ' || !!' + n2 + ')');
-        } else if (f === '||') {
-          nstack.push('(String(' + n1 + ') + String(' + n2 + '))');
-        } else if (f === '==') {
-          nstack.push('(' + n1 + ' === ' + n2 + ')');
-        } else if (f === '!=') {
-          nstack.push('(' + n1 + ' !== ' + n2 + ')');
-        } else {
-          nstack.push('(' + n1 + ' ' + f + ' ' + n2 + ')');
-        }
-      } else {
-        nstack.push('(' + n1 + ' ' + f + ' ' + n2 + ')');
-      }
-    } else if (type === IOP3) {
-      n3 = nstack.pop();
-      n2 = nstack.pop();
-      n1 = nstack.pop();
-      f = item.value;
-      if (f === '?') {
-        nstack.push('(' + n1 + ' ? ' + n2 + ' : ' + n3 + ')');
-      } else {
-        throw new Error('invalid Expression');
-      }
-    } else if (type === IVAR) {
-      nstack.push(item.value);
-    } else if (type === IOP1) {
-      n1 = nstack.pop();
-      f = item.value;
-      if (f === '-' || f === '+') {
-        nstack.push('(' + f + n1 + ')');
-      } else if (toJS) {
-        if (f === 'not') {
-          nstack.push('(' + '!' + n1 + ')');
-        } else if (f === '!') {
-          nstack.push('fac(' + n1 + ')');
-        } else {
-          nstack.push(f + '(' + n1 + ')');
-        }
-      } else if (f === '!') {
-        nstack.push('(' + n1 + '!)');
-      } else {
-        nstack.push('(' + f + ' ' + n1 + ')');
-      }
-    } else if (type === IFUNCALL) {
-      var argCount = item.value;
-      var args = [];
-      while (argCount-- > 0) {
-        args.unshift(nstack.pop());
-      }
-      f = nstack.pop();
-      nstack.push(f + '(' + args.join(', ') + ')');
-    } else if (type === IMEMBER) {
-      n1 = nstack.pop();
-      nstack.push(n1 + '.' + item.value);
-    } else if (type === IEXPR) {
-      nstack.push('(' + expressionToString(item.value, toJS) + ')');
-    } else {
-      throw new Error('invalid Expression');
-    }
-  }
-  if (nstack.length > 1) {
-    throw new Error('invalid Expression (parity)');
-  }
-  return nstack[0];
-}
 
 Expression.prototype.toString = function () {
   return expressionToString(this.tokens, false);
---
+++ src/expression-to-string.js
@@ -0,0 +1,97 @@
+import { INUMBER, IOP1, IOP2, IOP3, IVAR, IFUNCALL, IEXPR, IMEMBER } from './instruction';
+
+export default function expressionToString(tokens, toJS) {
+  var nstack = [];
+  var n1, n2, n3;
+  var f;
+  for (var i = 0, L = tokens.length; i < L; i++) {
+    var item = tokens[i];
+    var type = item.type;
+    if (type === INUMBER) {
+      if (typeof item.value === 'number' && item.value < 0) {
+        nstack.push('(' + item.value + ')');
+      } else {
+        nstack.push(escapeValue(item.value));
+      }
+    } else if (type === IOP2) {
+      n2 = nstack.pop();
+      n1 = nstack.pop();
+      f = item.value;
+      if (toJS) {
+        if (f === '^') {
+          nstack.push('Math.pow(' + n1 + ', ' + n2 + ')');
+        } else if (f === 'and') {
+          nstack.push('(!!' + n1 + ' && !!' + n2 + ')');
+        } else if (f === 'or') {
+          nstack.push('(!!' + n1 + ' || !!' + n2 + ')');
+        } else if (f === '||') {
+          nstack.push('(String(' + n1 + ') + String(' + n2 + '))');
+        } else if (f === '==') {
+          nstack.push('(' + n1 + ' === ' + n2 + ')');
+        } else if (f === '!=') {
+          nstack.push('(' + n1 + ' !== ' + n2 + ')');
+        } else {
+          nstack.push('(' + n1 + ' ' + f + ' ' + n2 + ')');
+        }
+      } else {
+        nstack.push('(' + n1 + ' ' + f + ' ' + n2 + ')');
+      }
+    } else if (type === IOP3) {
+      n3 = nstack.pop();
+      n2 = nstack.pop();
+      n1 = nstack.pop();
+      f = item.value;
+      if (f === '?') {
+        nstack.push('(' + n1 + ' ? ' + n2 + ' : ' + n3 + ')');
+      } else {
+        throw new Error('invalid Expression');
+      }
+    } else if (type === IVAR) {
+      nstack.push(item.value);
+    } else if (type === IOP1) {
+      n1 = nstack.pop();
+      f = item.value;
+      if (f === '-' || f === '+') {
+        nstack.push('(' + f + n1 + ')');
+      } else if (toJS) {
+        if (f === 'not') {
+          nstack.push('(' + '!' + n1 + ')');
+        } else if (f === '!') {
+          nstack.push('fac(' + n1 + ')');
+        } else {
+          nstack.push(f + '(' + n1 + ')');
+        }
+      } else if (f === '!') {
+        nstack.push('(' + n1 + '!)');
+      } else {
+        nstack.push('(' + f + ' ' + n1 + ')');
+      }
+    } else if (type === IFUNCALL) {
+      var argCount = item.value;
+      var args = [];
+      while (argCount-- > 0) {
+        args.unshift(nstack.pop());
+      }
+      f = nstack.pop();
+      nstack.push(f + '(' + args.join(', ') + ')');
+    } else if (type === IMEMBER) {
+      n1 = nstack.pop();
+      nstack.push(n1 + '.' + item.value);
+    } else if (type === IEXPR) {
+      nstack.push('(' + expressionToString(item.value, toJS) + ')');
+    } else {
+      throw new Error('invalid Expression');
+    }
+  }
+  if (nstack.length > 1) {
+    throw new Error('invalid Expression (parity)');
+  }
+  return nstack[0];
+}
+
+function escapeValue(v) {
+  if (typeof v === 'string') {
+    return JSON.stringify(v).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
+  }
+  return v;
+}

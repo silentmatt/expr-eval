b7d35ecebe8fe134d40dc07bed4805578985dcac
Matthew Crumley <email@matthewcrumley.com>
2017-06-10 18:56:35 -0400

    1.0.1

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.exprEval = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.exprEval = factory());
 }(this, (function () { 'use strict';
 
 /*!
@@ -972,31 +972,16 @@ TokenStream.prototype.parseError = function (msg) {
   throw new Error('parse error [' + (this.line + 1) + ':' + (this.column + 1) + ']: ' + msg);
 };
 
-var unaryInstructionCache = {};
 function unaryInstruction(value) {
-  var inst = unaryInstructionCache[value];
-  if (!inst) {
-    inst = unaryInstructionCache[value] = new Instruction(IOP1, value);
-  }
-  return inst;
+  return new Instruction(IOP1, value);
 }
 
-var binaryInstructionCache = {};
 function binaryInstruction(value) {
-  var inst = binaryInstructionCache[value];
-  if (!inst) {
-    inst = binaryInstructionCache[value] = new Instruction(IOP2, value);
-  }
-  return inst;
+  return new Instruction(IOP2, value);
 }
 
-var ternaryInstructionCache = {};
 function ternaryInstruction(value) {
-  var inst = ternaryInstructionCache[value];
-  if (!inst) {
-    inst = ternaryInstructionCache[value] = new Instruction(IOP3, value);
-  }
-  return inst;
+  return new Instruction(IOP3, value);
 }
 
 function ParserState(parser, tokenStream) {

d7f8884c1ae81f39b4b73cbbd3f27aed8d305a23
Matthew Crumley <email@matthewcrumley.com>
2017-09-06 13:42:33 -0400

    Build files for version 1.1.1

--- dist/bundle.js
+++ dist/bundle.js
@@ -311,8 +311,13 @@ function getSymbols(tokens, symbols, options) {
   for (var i = 0; i < tokens.length; i++) {
     var item = tokens[i];
     if (item.type === IVAR && !contains(symbols, item.value)) {
-      if (prevVar !== null || !withMembers) {
+      if (!withMembers) {
         symbols.push(item.value);
+      } else if (prevVar !== null) {
+        if (!contains(symbols, prevVar)) {
+          symbols.push(prevVar);
+        }
+        prevVar = item.value;
       } else {
         prevVar = item.value;
       }

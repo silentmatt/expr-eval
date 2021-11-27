b9c4253634c1108479bbc0153cfaab92d628cf42
Joe Pea <trusktr@gmail.com>
2021-11-27 07:44:29 -0800

    fix incorrect return types

--- parser.d.ts
+++ parser.d.ts
@@ -65,14 +65,14 @@ export class Parser {
     functions: any;
     consts: any;
     parse(expression: string): Expression;
-    evaluate(expression: string, values?: Value): number;
+    evaluate(expression: string, values?: Value): string;
     static parse(expression: string): Expression;
-    static evaluate(expression: string, values?: Value): number;
+    static evaluate(expression: string, values?: Value): string;
 }
 
 export interface Expression {
     simplify(values?: Value): Expression;
-    evaluate(values?: Value): any;
+    evaluate(values?: Value): string;
     substitute(variable: string, value: Expression | string | number): Expression;
     symbols(options?: { withMembers?: boolean }): string[];
     variables(options?: { withMembers?: boolean }): string[];
